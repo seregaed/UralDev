5fc63857b2e53d5089146513c6f0b58e4c12cdce
seregaed <edalin2@gmail.com>
2016-02-20 20:57:06 +0500

    Update venv.sh
    add comments

--- deploy/vagrant/venv.sh
+++ deploy/vagrant/venv.sh
@@ -1,18 +1,18 @@
-#!/bin/bash
+#!/bin/bash 
 
-set -e
+set -e #exit if error
 
-echo "cd src" >> /home/vagrant/.bashrc
+echo "cd src" >> /home/vagrant/.bashrc #print directory
 echo "cd src" >> /home/vagrant/.zprofile
 
 
 echo "Create python virtual environment"
-pyvenv-3.5 "/home/vagrant/env"
-source "/home/vagrant/env/bin/activate"
-pip install -U pip
+pyvenv-3.5 "/home/vagrant/env"  #create virtual environment with python 3.5
+source "/home/vagrant/env/bin/activate" #start second script in this 
+pip install -U pip  #install (upgrade?) pip
 echo 'source "/home/vagrant/env/bin/activate"' >> /home/vagrant/.bashrc
 echo 'source "/home/vagrant/env/bin/activate"' >> /home/vagrant/.zprofile
 
 echo "Install python packages"
-cd "/home/vagrant/src"
-pip install -e .
+cd "/home/vagrant/src" #change direcrtory
+pip install -e . #start second script in this directrory, install pip-packages 
