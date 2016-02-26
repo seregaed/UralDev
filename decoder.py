add6e4f915a7da01c4aa53955e8ee8343a1fee38
seregaed <edalin2@gmail.com>
2016-02-26 23:43:09 +0500

    Update decoder.py
    still incorrect

--- decoder.py
+++ decoder.py
@@ -3,24 +3,38 @@ import json
 def convert(fname_in, fname_out):
     fieldnames = ("name", "ru", "en", "synonims", "comments", "children")
  
-    with open(fname_in, "rt", encoding="utf-8") as fin, \
-            open(fname_out, "wt", encoding="utf-8") as fout:
+    with open(fname_in, "r") as fin, open(fname_out, "w") as fout:
  
         fout.write("[\n")
-        data = fin.readlines()  
-        print(data[0])
-        
-        for obj in data:
-            if obj == " ":
-                continue
-            elif obj[0] == " ":
-                continue
-            elif obj[1] == " ":
-                fout.write(json.dumps({synonims:obj}, ensure_ascii=False, indent=4) + "\n")                
+                
+        for line in fin:
+            count = 0
+            if line == " ":
+                count = 0
+                line = fin.readline()
+            elif line == "\n":
+                count = 0
+                line = fin.readline()
+            elif line[1:2] == " ":
+                if line == "\n":
+                    count = 0
+                    line = fin.readline()
+                elif line[3:4] == " ":
+                    if count == 1:
+                        fout.write(json.dumps({"children":line[3:-1]}, ensure_ascii=False, indent=4) + "\n")
+                        count = 0
+                        line = fin.readline()
+                else:
+                    fout.write(json.dumps({"synonims":line[1:-1]}, ensure_ascii=False, indent=4) + "\n")
+                    line = fin.readline()
+            
             else:
-                fout.write(json.dumps({"name":obj[:-1]}, ensure_ascii=False, indent=4) + "\n")
+                fout.write(json.dumps({"name":line[:-1]}, ensure_ascii=False, indent=4) + "\n")
+                line = fin.readline()
+        fout.write("]")
+            
         
 
-                fout.write("]")
+
 convert("Lung", "test1")
 #{name: {ru: '', en: ''}, synonims: ['', ''...], comments: [''...], children: [{}, {}...]}
