1af5b90db6f30b9c7e31c790d0cc62675e6e11d1
seregaed <edalin2@gmail.com>
2016-03-03 22:18:20 +0500

    Update decoder.py
    something new, but still incorrect

--- decoder.py
+++ decoder.py
@@ -1,39 +1,29 @@
 import json
+import re
+stack = []
+current = {}
 
 def convert(fname_in, fname_out):
     fieldnames = ("name", "ru", "en", "synonims", "comments", "children")
  
     with open(fname_in, "r") as fin, open(fname_out, "w") as fout:
- 
+        
         fout.write("[\n")
                 
         for line in fin:
-            count = 0
-            if line == " ":
-                count = 0
+            result = re.findall('\s', line)
+            if len(result) == 1:
                 line = fin.readline()
-            elif line == "\n":
-                count = 0
+            elif len(result) >= 2:
+                current["'name':'en'"] = line.strip()
+                fout.write(json.dumps(current, ensure_ascii=False, indent=4))
                 line = fin.readline()
-            elif line[1:2] == " ":
-                if line == "\n":
-                    count = 0
+                result = re.findall('\s', line)
+                if len(result) == 1:
                     line = fin.readline()
-                elif line[3:4] == " ":
-                    if count == 1:
-                        fout.write(json.dumps({"children":line[3:-1]}, ensure_ascii=False, indent=4) + "\n")
-                        count = 0
-                        line = fin.readline()
                 else:
-                    fout.write(json.dumps({"synonims":line[1:-1]}, ensure_ascii=False, indent=4) + "\n")
-                    line = fin.readline()
-            
-            else:
-                fout.write(json.dumps({"name":line[:-1]}, ensure_ascii=False, indent=4) + "\n")
-                line = fin.readline()
-        fout.write("]")
-            
-        
+                    current["'name':'ru'"] = line.strip()
+                    fout.write(json.dumps(current, ensure_ascii=False, indent=4))
 
 
 convert("Lung", "test1")
